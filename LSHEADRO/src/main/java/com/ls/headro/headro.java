e46d204de9b2b383268cff8bf4dc62f15f61bb99
* <harshsandeep23@gmail.com>
2023-02-04 11:43:53 +0530

    initial commit' git branch -M master
    git

---
+++ LSHEADRO/src/main/java/com/ls/headro/headro.java
@@ -0,0 +1,12 @@
+package com.ls.headro;
+
+public class headro {
+
+    public headro(){
+    }
+    
+    public String getHeadro(){
+        return "hello headro";
+    }
+
+}
---
+++ app/src/main/java/com/ls/headroexample/MainClass.java
@@ -0,0 +1,9 @@
+package com.ls.headroexample;
+
+public class MainClass {
+
+    public void main(){
+        String a = new headro().getHeadro();
+    }
+
+}
